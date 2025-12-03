983faacb046a9370296dc391b3227ff08994180e
2196447160@qq.com <wanjie@wanjiedeMacBook-Air.local>
2025-12-03 22:25:14 +0800

    提交到dev分支

--- demo1/src/main/java/com/wj/demo1/Demo1Application.java
+++ demo1/src/main/java/com/wj/demo1/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
-        #start the project
+        // start the project
         SpringApplication.run(Demo1Application.class, args);
     }
 
---
+++ demo1/src/main/java/com/wj/demo1/Hello.java
@@ -0,0 +1,7 @@
+package com.wj.demo1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
